3951b44c9bbb6e09afa6ff35f89307c181d70779
jariasca9 <m212t0436@misantla.tecnm.mx>
2026-02-17 02:46:53 -0600

    Update styles for background and add carousel component with scrolling animation

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './components/Home'
+import Carousel from './components/Carousel'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <Carousel />
   </StrictMode>,
 )
